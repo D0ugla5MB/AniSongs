c2bbfb93e9042ffa239d7c8202d217dba183350b
DMB <douglasmbdev@gmail.com>
2024-12-15 00:59:16 -0300

    FIXED BUG: recording data are being overwritten;            reason: the lack of knowledge of the            Py's dict reference update

--- animes_scrape/animes_scrape/spiders/anime_op_ed.py
+++ animes_scrape/animes_scrape/spiders/anime_op_ed.py
@@ -1,7 +1,7 @@
+from copy import deepcopy
 import scrapy
-import re
 import json
-from anisong.utils.format_data import (extract_substrings, parse_song_info, put_placeholders, trim_trailing_spaces)
+from anisong.utils.format_data import (parse_song_info, trim_trailing_spaces)
 from anisong.utils.files_config import (
     get_css_selectors,
     get_json_template,
@@ -59,13 +59,12 @@ class AnimeOpEdSpider(scrapy.Spider):
         
         sections = response.xpath(css_selectos.get('sections_xpath'))
         for section in sections:
+            td_elements = section.xpath(css_selectos.get('td_xpath'))
             is_opening = css_selectos.get('op') in section.get().lower()
             song['type'] = "opening" if is_opening else "ending"
-            td_elements = section.xpath(css_selectos.get('td_xpath'))
             for td in td_elements:
                 #new_str = trim_trailing_spaces(td.xpath("string(.)").get())
                 track_content = trim_trailing_spaces(td.xpath("string(.)").get())
-                #pause_coderun()
                 track_content = parse_song_info(regex_patterns, track_content)
                 if not track_content:
                     self.logger.error(f"Error at '\033[92m'track_content'\033[0m' extraction for anime: {anime['anime_roman_title']}, song data is missing.")
@@ -74,22 +73,20 @@ class AnimeOpEdSpider(scrapy.Spider):
                 try:
                     song_data = track_content['song']
                     artist_data = track_content['artist']
-                    
-                    song['num'], song['roman_name'], song['jp_name'] = song_data
-                    artist['roman_name'], artist['jp_name'] = artist_data
-                    track_data = {'song': song, 'artist': artist}
-                    
-                 #   pause_coderun()
-                    if song['type'] == "opening":
-                            try:
-                                op_list.append(track_data)
-                            except Exception as e:
-                                self.logger.error(f"Error appending to 'op_list' for anime: {anime['anime_roman_title']}, {track_data}, error: {str(e)}")
+                        
+                    song_instance = deepcopy(song)
+                    artist_instance = deepcopy(artist)
+
+                    song_instance['num'], song_instance['roman_name'], song_instance['jp_name'] = song_data.values()
+                    artist_instance['roman_name'], artist_instance['jp_name'] = artist_data.values()
+
+                    track_data = {'song': song_instance, 'artist': artist_instance}
+
+                    if song_instance['type'] == "opening":
+                        op_list.append(track_data)
                     else:
-                            try:
-                                ed_list.append(track_data)
-                            except Exception as e:
-                                self.logger.error(f"Error appending to 'ed_list' for anime: {anime['anime_roman_title']}, track_data: {track_data}, error: {str(e)}")
+                        ed_list.append(track_data)
+
 
                 except Exception as e:
                     self.logger.error(f"Error processing song data for anime: {anime['anime_roman_title']}, song content: {track_content}, error: {str(e)}")
